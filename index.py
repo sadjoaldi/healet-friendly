e00dc8ff89b15c14f00c7da0b7a86de159b30a38
Sadjoaldi <alsannealdi@outlook.com>
2023-05-20 21:44:43 +0200

    tjr carrousel

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 
 
-nbJours = int(input())
-nbHeuresMarche = nbJours * 16
-nbRepetitions = nbHeuresMarche * 60 * 60
-print(nbRepetitions)
+nbVariations = int(input())
+
+alMontees = 0
+alDescentes = 0
+
+for loop in range(nbVariations):
+    varAltitude = int(input())
+    if (varAltitude < 0):
+        alDescentes -= varAltitude
+    else:
+        alMontees += varAltitude
+
+print(alMontees)
+print(alDescentes)
